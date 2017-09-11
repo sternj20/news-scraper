16fdcdaf5cd32534f4e5e4ddb7002acc7e0fcd81
sternj20 <jessesternmusic@gmail.com>
2017-09-10 21:22:56 -0700

    changed variables to es6 syntax using const and let. added show saved articles button and route that displays saved articles

--- server.js
+++ server.js
@@ -8,40 +8,35 @@
 
 // Good luck!
 
-// Dependencies
+
+
+
 var express = require("express");
-var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-// Requiring our Note and Article models
-// var Note = require("./models/Note.js");
-var Article = require("./models/Article.js");
-// Our scraping tools
-var exphbs = require("express-handlebars");
-var request = require("request");
-var cheerio = require("cheerio");
-var methodOverride = require('method-override');
+const body = require("body-parser");
+const path = require("path");
+const app = express();
+const exphbs = require("express-handlebars");
+const router = require(path.join(__dirname, "controllers", "controller.js"));
+const methodOverride = require('method-override');
+const mongoose = require("mongoose");
+
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
 
-
-// Initialize Express
-var app = express();
 
 // Set Handlebars as the default templating engine.
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
-
-// Use morgan and body parser with our app
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+app.use(body.json()); // support json encoded bodies
+app.use(body.urlencoded({ extended: false })); // support encoded bodies
 
 app.use(methodOverride('_method'))
 
 // Make public a static dir
 app.use(express.static("public"));
+app.use("/", router);
 
 // Database configuration with mongoose
 mongoose.connect("mongodb://localhost/newsdb");
@@ -56,70 +51,6 @@ db.on("error", function(error) {
 db.once("open", function() {
   console.log("Mongoose connection successful.");
 });
-
-
-app.get("/scrape", function(req, res){
-  // Make a request call to grab the HTML body from the site of your choice
-  request("http://www.npr.org/sections/news/", function(error, response, html) {
-
-  // Load the HTML into cheerio and save it to a variable
-  // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-  const $ = cheerio.load(html);
-
-  const result = {};
-
-  // Select each element in the HTML body from which you want information.
-  // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-  // but be sure to visit the package's npm page to see how it works
-  $(".has-image").each(function(i, element) {
-    result.title = $(element).children(".item-info").children(".title").children("a").text();
-    result.link = $(element).children(".item-info").children(".title").children("a").attr("href");
-    result.timeAndSummary = $(element).children(".item-info").children(".teaser").children("a").text();
-          // Using our Article model, create a new entry
-      // This effectively passes the result object to the entry (and the title and link)
-      var entry = new Article(result);
-
-      // Now, save that entry to the db
-      entry.save(function(err, doc) {
-        // Log any errors
-        if (err) {
-          console.log(err);
-        }
-        // Or log the doc
-        else {
-          console.log(doc);
-        }
-      });
-
-    });
-  // Tell the browser that we finished scraping the text
-  res.redirect("./")
-});
-});
-
-app.get("/", function(req, res){
-  Article.find({}, function(error, doc){
-    const hbsObject = {};
-    if(error){
-      console.log(error);
-    } else {
-      hbsObject.article = doc;
-      res.render("index", hbsObject);
-    }
-  });
-});
-
-
-app.put("/:id", function(req, res) {
-
-  Article.findOneAndUpdate({_id: req.params.id}, {$set:{
-    saved: true
-  }}, function(error, result){
-    res.redirect("/");
-  })
-});
-
-
 
 // Listen on port 3000
 app.listen(3000, function() {
