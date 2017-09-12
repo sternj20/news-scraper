e8cac2835a955c943e0c6b107303515bbfe7ac2e
sternj20 <jessesternmusic@gmail.com>
2017-09-11 23:56:09 -0700

    added renderModal function to render the comments in the modal showing the article comments

--- public/assets/js/saved.js
+++ public/assets/js/saved.js
@@ -1,13 +1,33 @@
+const renderModal = function(arr){
+	$(".modal-body").empty();
+	arr.forEach(function(element){
+		let div = $('<div class ="panel panel-default">');
+		let divBody = $('<div class="panel-body">');
+		divBody.text(element.comment);
+		div.append(divBody);
+		$(".modal-body").append(div);
+		console.log(element);
+	});
+};
+
+
+
 $(document).ready(function() {
 	let articleID;
+	let activeArticleComments;
+	//when new add new comment button is clicked
 	$(".newComment").on("click", function(){
+		//grab the unique id of the article that user wants to add comment on
 		articleID = $(this).attr("data-id");
 		$.ajax({
+			//concatenate this id onto the path so we can reference it on the backend
 			url:'/api/articles/newcomment/' + articleID,
 			data:{},
 			method:"GET",
 			success:function(data){
-				console.log(data)
+				activeArticleComments = data;
+				renderModal(data);
+				console.log(activeArticleComments)
 			}
 		});
 	});
